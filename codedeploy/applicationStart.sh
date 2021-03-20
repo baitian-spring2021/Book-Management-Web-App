31b091630ebce7fee037c42a65c519bc3225e3ec
Viola Bai <bai.tian@northeastern.edu>
2021-03-20 16:02:50 -0700

    fixing logger

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd '/home/ubuntu/webapp'
-sudo java -jar ~/webappone/target/webappone-0.0.1-SNAPSHOT.jar > /home/ubuntu/csye6225.txt 2> /home/ubuntu/csye6225.txt < /home/ubuntu/csye6225.txt &
+sudo java -jar ~/webappone/target/webappone-0.0.1-SNAPSHOT.jar > /home/ubuntu/csye6225.log 2> /home/ubuntu/csye6225.log < /home/ubuntu/csye6225.log &
