7b85d6f51bd728bc06479badd014bb11e53de397
Viola Bai <bai.tian@northeastern.edu>
2021-04-03 12:37:33 -0700

    fixing deletion userNotFound

--- webappone/src/main/java/neu/csye6225/webappone/service/BookServiceImpl.java
+++ webappone/src/main/java/neu/csye6225/webappone/service/BookServiceImpl.java
@@ -2,9 +2,13 @@ package neu.csye6225.webappone.service;
 
 import com.timgroup.statsd.StatsDClient;
 import neu.csye6225.webappone.aws.SNSService;
+import neu.csye6225.webappone.controller.BookController;
 import neu.csye6225.webappone.dao.BookDao;
 import neu.csye6225.webappone.dao.UserDao;
 import neu.csye6225.webappone.pojo.Book;
+import neu.csye6225.webappone.pojo.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +24,7 @@ public class BookServiceImpl implements BookService {
     private StatsDClient statsd;
     @Autowired
     private SNSService snsService;
+    private Logger logger = LoggerFactory.getLogger(BookServiceImpl.class);
 
     @Override
     public Book save(Book book) {
@@ -61,7 +66,11 @@ public class BookServiceImpl implements BookService {
         bookDao.deleteById(id);
         statsd.recordExecutionTime("DB Response Time - Delete Book", System.currentTimeMillis() - startTime);
         Book book = bookDao.findById(id);
-        String userEmail = userDao.findById(book.getUser_id()).getUsername();
+        String userId = book.getUser_id();
+        logger.info(userId);
+        User user = userDao.findById(userId);
+        String userEmail = user.getUsername();
+        logger.info(userEmail);
         snsService.postToTopic("DELETE", userEmail, book.getId(), book.getTitle(), book.getAuthor());
     }
 
